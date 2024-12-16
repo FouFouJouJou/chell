bce13a51480315ca860662ad8a897882353a49f9
FouFouJouJou <firasjaballiftw@gmail.com>
2024-12-17 00:02:46 +0100

    pipe parsing

--- headers/parser.h
+++ headers/parser.h
@@ -2,13 +2,38 @@
 #define __PARSER_H__
 #include <tree.h>
 
+enum node_type_t {
+   NODE_PIPE
+  ,NODE_AND
+  ,NODE_SEMI_COLON
+  ,NODE_REDIR
+  ,NODE_CMD
+  ,NODE_UNSUPPORTED
+};
+
+struct node_t {
+  void *data;
+  enum node_type_t type;
+  struct node_t *right;
+  struct node_t *left;
+};
+
 struct cmd_t {
   char *executable;
   char **argv;
   int argc;
 };
 
-void printf_tree(struct node_t *head, int level);
+struct pipe_t {
+  int pipe[2];
+};
+
+typedef void(*printf_node_func)(struct node_t node);
+void printf_node(struct node_t node);
 void run(struct node_t *head);
 struct node_t *parse(char *line, size_t len);
+
+void push_head(struct node_t *node, struct node_t **head);
+void printf_tree(struct node_t *node, int level, printf_node_func printf_func);
+
 #endif
--- source/parser.c
+++ source/parser.c
@@ -7,7 +7,42 @@
 #include <parser.h>
 #define ASSERT_T(x, t) assert((x)->type == t)
 
-void printf_cmd_node(struct cmd_t cmd) {
+struct node_t *create_empty_node() {
+  struct node_t *node=calloc(1, sizeof(struct node_t));
+  node->data=0;
+  node->type=NODE_UNSUPPORTED;
+  node->right=node->left=0;
+  return node;
+}
+
+struct node_t *create_cmd_node(struct cmd_t *cmd) {
+  struct node_t *node = calloc(1, sizeof(struct node_t));
+  node->type = NODE_CMD;
+  node->data=cmd;
+  return node;
+}
+
+void push_head(struct node_t *node, struct node_t **head) {
+  if(*head == 0) {
+    *head=node;
+    return;
+  }
+  node->left=*head;
+  *head=node;
+}
+
+void printf_tree(struct node_t *node, int level, printf_node_func printf_func) {
+  if(node == 0) {
+    return;
+  }
+  for(int i=0; i<level; ++i) printf("  ");
+  printf_func(*node);
+  printf_tree(node->left, level+1, printf_func);
+  printf_tree(node->right, level+1, printf_func);
+}
+
+void printf_cmd_node(struct node_t node) {
+  struct cmd_t cmd=*(struct cmd_t *)node.data;
   printf("CMD: (%s, %d, [", cmd.executable, cmd.argc);
   for(int i=0; i<cmd.argc; ++i) {
     printf("%s", cmd.argv[i]);
@@ -16,7 +51,36 @@ void printf_cmd_node(struct cmd_t cmd) {
   printf("])\n");
 }
 
-void printf_tree(struct node_t *head, int level) {
+void printf_node(struct node_t node) {
+  switch(node.type) {
+    case NODE_PIPE:
+      printf("|\n");
+      break;
+    case NODE_AND:
+      printf("&&\n");
+      break;
+    case NODE_SEMI_COLON:
+      printf(";\n");
+      break;
+    case NODE_REDIR:
+      break;
+    case NODE_CMD:
+      printf_cmd_node(node);
+      break;
+    case NODE_UNSUPPORTED:
+      break;
+    default:
+      printf("nope\n");
+      break;
+  }
+}
+
+struct node_t *create_pipe_node(struct node_t *left, struct node_t *right) {
+  struct node_t *pipe_node=calloc(1, sizeof(struct node_t));
+  pipe_node->type=NODE_PIPE;
+  pipe_node->right=right;
+  pipe_node->left=left;
+  return pipe_node;
 }
 
 size_t parse_cmd(struct token_t *tokens, struct node_t *node) {
@@ -27,13 +91,17 @@ size_t parse_cmd(struct token_t *tokens, struct node_t *node) {
   struct token_t *token=tokens;
   ASSERT_T(token, TOKEN_STRING);
   cmd->executable=token->literal;
-  while(token->type != TOKEN_PIPE && token->type != TOKEN_EOC && token->type != TOKEN_AND && token->type != TOKEN_SEMI_COLON) {
+  while(token->type != TOKEN_PIPE 
+    && token->type != TOKEN_EOC 
+    && token->type != TOKEN_AND 
+    && token->type != TOKEN_SEMI_COLON
+  ) {
     ASSERT_T(token, TOKEN_STRING);
     cmd->argc++;
     cmd->argv=realloc(cmd->argv, (cmd->argc-1)*sizeof(char*));
     cmd->argv[cmd->argc-1]=token->literal;
     token++;
-  } 
+  }
   return token-tokens;
 }
 
@@ -42,25 +110,28 @@ size_t parse_pipe(struct token_t *tokens, struct node_t *node) {
 }
 
 struct node_t *build_tree(struct token_t *tokens) {
-  struct node_t *head=calloc(1, sizeof(struct node_t));
+  struct node_t *head=0;
   struct token_t *token=tokens;
+  int stack_idx=0;
+  struct node_t *stack[20];
   while(token->type != TOKEN_EOC) {
     if(token->type == TOKEN_STRING) {
-      size_t read=parse_cmd(token, head);
-      printf_cmd_node(*((struct cmd_t*)head->data));
+      struct node_t *node=calloc(1, sizeof(struct node_t));
+      size_t read=parse_cmd(token, node);
+      stack[stack_idx++]=node;
+      assert(stack_idx==1);
       token+=read;
     }
+
     if(token->type == TOKEN_PIPE) {
-      printf("PIPE\n");
       token++;
-    }
-    if(token->type == TOKEN_AND) {
-      printf("AND\n");
-      token++;
-    }
-    if(token->type == TOKEN_SEMI_COLON) {
-      printf("SEMI_COLON\n");
-      token++;
+      struct node_t *right_cmd=calloc(1, sizeof(struct node_t));
+      size_t read=parse_cmd(token, right_cmd);
+      struct node_t *left_cmd=stack[stack_idx-1];
+      struct node_t *pipe_node=create_pipe_node(left_cmd, right_cmd);
+      push_head(pipe_node, &head);
+      stack[stack_idx]=pipe_node;
+      token+=read;
     }
   }
   return head;
@@ -72,5 +143,6 @@ void run(struct node_t *head) {
 struct node_t *parse(char *line, size_t len) {
   struct token_t *tokens=lex(line, len);
   struct node_t *head=build_tree(tokens);
+  printf_tree(head, 0, printf_node);
   return head;
 }
