9fffd848b38a270078e3fc2866b2515cd76d9c84
FouFouJouJou <firasjaballiftw@gmail.com>
2024-12-18 07:30:31 +0100

    executing a pipe command

--- source/exec.c
+++ source/exec.c
@@ -1,7 +1,59 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <parser.h>
 #include <exec.h>
+#include <sys/wait.h>
 
-void run(struct node_t *tree) {
-  printf_tree(tree, 0, printf_node);
+void run(struct node_t *node) {
+  switch(node->type) {
+    case NODE_PIPE: {
+      int p[2];
+      if(pipe(p) == -1) exit(71);
+      int status_left, status_right;
+      pid_t left_process=fork();
+      if(left_process == 0) {
+        if(dup2(p[1], STDOUT_FILENO) == -1) exit(71);
+        run(node->left);
+        return;
+      }
+
+
+      pid_t right_process=fork();
+      if(right_process == 0) {
+        if(dup2(p[0], STDIN_FILENO) == -1) exit(71);
+        run(node->right);
+        return;
+      }
+      waitpid(left_process, &status_left, 1);
+      waitpid(right_process, &status_right, 1);
+      break;
+    }
+    case NODE_AND:
+      break;
+    case NODE_SEMI_COLON: {
+      pid_t left_process=fork();
+      int status_left, status_right;
+      if(left_process == 0) {
+        run(node->left);
+        return;
+      }
+      waitpid(left_process, &status_left, 0);
+      pid_t right_process=fork();
+      if(right_process == 0) {
+        run(node->right);
+        return;
+      }
+      waitpid(right_process, &status_right, 0);
+      break;
+    }
+
+    case NODE_REDIR:
+      break;
+    case NODE_CMD: {
+      struct cmd_t *cmd=(struct cmd_t *)node->data;
+      if(execvp(cmd->executable, cmd->argv) == -1) exit(72);
+      break;
+    }
+  }
 }
--- source/main.c
+++ source/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <parser.h>
 #include <exec.h>
+#include <sys/wait.h>
 
 int main(int argc, char **argv) {
   struct node_t *head=parse(argv[1], strlen(argv[1]));
